9a10ea38edb91278c0ed3db4b088b4db445bb639
user1 <user1@163.com>
2019-07-27 09:26:34 +0800

    wyh实现了上一章下一章

--- body/views.py
+++ body/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from body import models
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 def index(request):
     return render(request, 'index.html')
 # Create your views here.
@@ -7,15 +9,34 @@ def index(request):
 def bookrack(request):
     return render(request, 'bookrack.html')
 
+
 def chapter(request):
-    return render(request, 'chapter.html')
+
+    all = models.Noval_room.objects.filter(book_foreign="http://www.xbiquge.la/14/14963/").order_by("book_chapter_id")
+    return render(request, 'chapter.html',{"all":all})
+
+
 
 def register(request):
     return render(request, 'register.html')
 
 def article(request):
+    book = "http://www.xbiquge.la/14/14963/"
+    chapter = '0'
+    info = models.Noval_room.objects.filter(book_foreign=book,book_chapter_id=chapter).values()
+    info = list(info)[0]
+    info['book_content'] = str(info['book_content']).replace('\r','')
+    return render(request, 'article.html', {"info": info})
 
-    return render(request, 'article.html')
+@csrf_exempt
+def article_ajax(request):
+    paramter = request.POST
+    book = paramter.get('book')
+    chapter = paramter.get('chapter')
+    info = models.Noval_room.objects.filter(book_foreign=book,book_chapter_id=chapter).values()
+    info = list(info)[0]
+    info['book_content'] = str(info['book_content']).replace('\r','')
+    return JsonResponse(info)
 
 def ranking_list(request):
 
